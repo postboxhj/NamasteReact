e4eac3c4c35dedbc8a9df9bc197f4cc42efb853f
Harish-sarkar <postboxhj@gmail.com>
2024-02-10 14:10:35 +0530

    Episode-1

---
+++ main.js
@@ -0,0 +1,10 @@
+const parent = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hello world!"),
+  React.createElement("h1", {}, "Hello JavaScript!"),
+  React.createElement("h1", {}, "Hello React!")
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
